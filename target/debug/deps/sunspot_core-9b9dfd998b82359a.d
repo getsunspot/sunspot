7aecb45a10f4a79e0f585d812f1ac0ba78ddd45d
retc3 <retc3@disroot.org>
2026-07-31 22:50:56 +0300

    ignore rust build artifacts

--- target/debug/deps/sunspot_core-9b9dfd998b82359a.d
+++
@@ -1,5 +0,0 @@
-D:\cl605\sunspot-org\sunspot\target\debug\deps\sunspot_core-9b9dfd998b82359a.d: crates\sunspot-core\src\lib.rs
-
-D:\cl605\sunspot-org\sunspot\target\debug\deps\libsunspot_core-9b9dfd998b82359a.rmeta: crates\sunspot-core\src\lib.rs
-
-crates\sunspot-core\src\lib.rs:
